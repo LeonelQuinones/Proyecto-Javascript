9950bb5c224185bb44e92bf3946d53d88f0d4fcc
leonelquinones <leonelquinones9@gmail.com>
2022-04-05 06:16:14 -0300

    detalles

--- script.js
+++ script.js
@@ -30,15 +30,17 @@ btnCarrito.addEventListener('click', () => {
     seccionCarrito.classList.toggle("active")
 })
 
+let teclados = document.querySelector(".teclados")
+
 obtenerProductos().then(productos => {
     productos.forEach((producto) => {
         listaDeProductos.innerHTML += `
             <div class="card bg-secondary producto" style="max-width: 20rem;">
                 <img src="./img/${producto.img}" class="card-img-top" alt=${producto.nombre}></img>
-                <div class="card-header titulo-producto">${producto.nombre}</div>
+                <div class="nombre card-header titulo-producto" id="nombre">${producto.nombre}</div>
                 <div class="card-body">
                     <h4 class="card-title">${producto.marca}</h4>
-                    <p class="card-text">Precio: $<span>${producto.precio}</span></p>
+                    <p class="precio card-text" id="precio">Precio: $<span id="span">${producto.precio}</span></p>
                     <p class="card-text">Stock: <span>${producto.stock}</span></p>
                     <button class="btn btn-primary btnAgregar" data-id="${producto.id}">Comprar</button>
                 </div>
@@ -46,17 +48,96 @@ obtenerProductos().then(productos => {
         `
     })
 
+    teclados.addEventListener('click', () => {
+
+    })
+
     let btnAgregar = document.querySelectorAll(".btnAgregar");
     
     btnAgregar.forEach((e) => {
         e.addEventListener('click', (e) => {
-            let cardPadre = e.target.parentElement.parentElement;
-            
+            let cardProducto = e.target.parentElement.parentElement;
+            sumarCarrito(cardProducto);
         })
     })
 })
 
 
+const sumarCarrito = (cardProducto) => {
+    let producto = {
+        nombre: cardProducto.querySelector(".nombre").textContent,
+        precio: Number(cardProducto.querySelector(".precio span").textContent),
+        cantidad: 1,
+        img: cardProducto.querySelector("img").src,
+        id: Number(cardProducto.querySelector("button").getAttribute("data-id"))
+    }
+
+    let productoEncontrado = carrito.find((elemento) => elemento.id === producto.id)
+
+    if (productoEncontrado) {
+        productoEncontrado.cantidad++
+    } else {
+        carrito.push(producto)
+    }
+
+    mostrarCarrito()
+};
+
+const mostrarCarrito = () => {
+    seccionCarrito.innerHTML = ""
+    carrito.forEach(producto => {
+        let {imagen, nombre, precio, cantidad, id} = producto;
+        seccionCarrito.innerHTML += `
+            <div class="card bg-secondary producto" style="max-width: 10rem;">
+                <img src="${imagen}" class="card-img-top" alt=${nombre}></img>
+                <div class="nombre card-header titulo-producto" id="nombre">${nombre}</div>
+                <div class="card-body">
+                    <p class="card-text">Cantidad: ${cantidad}</p>
+                    <p class="precio card-text" id="precio">Precio: $${precio}</p>
+                    <p class="card-text">Subtotal: $${precio * cantidad}</p>
+                    <button class="btn btn-primary btnRestar" data-id="${id}">-</button>
+                    <button class="btn btn-primary btnBorrar" data-id="${id}">Borrar</button>
+                </div>
+            </div>
+        `
+    })
+/*
+    let btnRestar = document.querySelector(".btnRestar");
+    let btnBorrar = document.querySelector(".btnBorrar");
+
+    btnRestar.addEventListener('click', (e) => {
+        restarProducto(e.target.getAttribute("data-id"))
+    })
+
+    btnBorrar.addEventListener('click', (e) => {
+        borrarProducto(e.target.getAttribute("data-id"))
+    })*/
+}
+/*
+const restarProducto = (productoRestar) => {
+    let productoEncontrado = carrito.find(elemento => elemento.id === Number(productoRestar))
+    if (productoEncontrado) {
+        productoEncontrado.cantidad--
+        if (productoEncontrado.cantidad === 0) {
+            productoEncontrado.cantidad = 1
+        }
+    }
+}
+*/
+
+const botonesCarrito = (e) => {
+    e.preventDefault()
+    carrito.addEventListener("click", (e) => {
+        if (e.target.classList.contains("btnRestar")) {
+            console.log("restar");
+        }
+        if (e.target.classList.contains("btnBorrar")) {
+            console.log("borrar");
+        }
+    })
+}
+
+botonesCarrito()
 
 
 
